3bc3ad2175573615534bfe9afb1aec896a5c4716
vincent <vincent@boohee.com>
2015-12-26 18:26:41 +0800

    Add core methods for SmartShardKey.

--- lib/smart_shard_key.rb
+++ lib/smart_shard_key.rb
@@ -1,5 +1,74 @@
 require "smart_shard_key/version"
 
 module SmartShardKey
-  # Your code goes here...
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    # smart_id total len
+    TOTAL_LEN = 64
+    # len of timestamp
+    TIMESTAMP_LEN = 36
+    # len of shard_id
+    SHARD_ID_LEN = 13
+    # len of sequence
+    SEQUENCE_LEN = 15
+
+    # return shard_id of the given key
+    # @param key[int]
+    # @return shard_id[int]
+    def shard_id_of_key(key)
+      key.to_i % (1 << SHARD_ID_LEN)
+    end
+
+    # return shard_id of the given id
+    # @param id[int] 64 bits id
+    # @return shard_id[int]
+    def shard_id_of_id(id)
+      (id >> SEQUENCE_LEN) & ((1 << SHARD_ID_LEN) - 1)
+    end
+
+    # generate smart id
+    # @param key[int]
+    # @param sequence[int] optional
+    # @param timestamp[float] optional
+    # @return smart id[int] 64 bits integer
+    def generate_id(key, sequence = nil, timestamp = nil)
+      scale_timestamp =
+        if timestamp
+          (timestamp * 10).to_i
+        else
+          (Time.now.to_f * 10).to_i
+        end
+
+      shard_id = shard_id_of_key(key)
+
+      # use random value for sequence without given
+      safe_sequence =
+        if sequence
+          sequence % (1 << SEQUENCE_LEN)
+        else
+          rand(1 << SEQUENCE_LEN)
+        end
+
+      (scale_timestamp << (TOTAL_LEN - TIMESTAMP_LEN)) |
+        (shard_id << SEQUENCE_LEN) |
+        safe_sequence
+    end
+
+    # split smart id to three segment: timestamp, shard_id and sequence
+    # @param id[int] smart id
+    # @return [timestamp[Time], shard_id[int], sequence[int]]
+    def split_id(id)
+      timestamp = (id >> (TOTAL_LEN - TIMESTAMP_LEN)) / 10.0
+
+      shard_id = (id >> SEQUENCE_LEN) & ((1 << SHARD_ID_LEN) - 1)
+
+      sequence = id & ((1 << SEQUENCE_LEN) - 1)
+
+      [Time.at(timestamp), shard_id, sequence]
+    end
+  end
+
 end
--- test/smart_shard_key_test.rb
+++ test/smart_shard_key_test.rb
@@ -1,11 +1,54 @@
 require 'test_helper'
 
+class SmartShardKeyTestModel
+  include SmartShardKey
+end
+
 class SmartShardKeyTest < Minitest::Test
+  def setup
+    @smart_id = 2342352325252323
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::SmartShardKey::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_get_shard_id_of_key
+    assert_equal 0, SmartShardKeyTestModel.shard_id_of_key(0)
+    assert_equal 102, SmartShardKeyTestModel.shard_id_of_key(102)
+    assert_equal 13, SmartShardKeyTestModel.shard_id_of_key(8192 + 13)
+
+    assert_equal 5, SmartShardKeyTestModel.shard_id_of_key("8197")
+  end
+
+  def test_generate_id
+    key0 = 124
+    sequence0 = 101
+    timestamp0 = 1451122080.3224
+    assert_equal 3895326173374054501,
+      SmartShardKeyTestModel.generate_id(key0, sequence0, timestamp0)
+
+    key0 = 12424
+    sequence0 = 101423532
+    timestamp0 = 1451121083.8915
+    assert_equal 3895323498549352876,
+      SmartShardKeyTestModel.generate_id(key0, sequence0, timestamp0)
+  end
+
+  def test_get_shard_id_of_id
+    assert_equal 124, SmartShardKeyTestModel.shard_id_of_id(3895326173374054501)
+    assert_equal 4232, SmartShardKeyTestModel.shard_id_of_id(3895323498549352876)
+  end
+
+  def test_split_id
+    timestamp0, shard_id0, sequence0 = SmartShardKeyTestModel.split_id(3895326173374054501)
+    assert_equal 124, shard_id0
+    assert_equal 101, sequence0
+    assert_equal 1451122080, timestamp0.to_i
+
+    timestamp1, shard_id1, sequence1 = SmartShardKeyTestModel.split_id(3895323498549352876)
+    assert_equal 4232, shard_id1
+    assert_equal 6572, sequence1
+    assert_equal 1451121083, timestamp1.to_i
   end
 end
